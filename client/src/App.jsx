52ec0dff65c7ac1e517b76504803a24f549a0697
Thaneshkumar-P <thaneshkumarp20ug0433@drngpit.ac.in>
2023-07-08 00:06:55 +0530

    initial

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap'
+
+
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App;
